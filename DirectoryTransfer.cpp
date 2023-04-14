21a172e9ec6415469c4bbce35833e52d2dca7b5a
HlebK 050541 <h.kokhanovsky@gmail.com>
2023-04-15 00:32:41 +0300

    The documentation updated. Some changes in placement of the code blocks

--- DirectoryTransfer.cpp
+++ DirectoryTransfer.cpp
@@ -7,14 +7,14 @@
 #include "ArchiveHandler.h"
 
 class DirectoryTransfer {
-public:
-    DirectoryTransfer(std::string hostname, int port, std::string username, std::string password, std::string remoteDir, std::string localDir);
-    bool syncRemoteToLocal();
-    bool syncLocalToRemote();
-
 private:
     FTPClient ftpClient;
     ArchiveHandler archiveHandler;
     std::string remoteDir;
     std::string localDir;
+
+public:
+    DirectoryTransfer(std::string hostname, int port, std::string username, std::string password, std::string remoteDir, std::string localDir);
+    bool syncRemoteToLocal();
+    bool syncLocalToRemote();
 };
